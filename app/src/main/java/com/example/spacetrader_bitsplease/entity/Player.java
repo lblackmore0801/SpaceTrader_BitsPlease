0912ac9557e666b08b472901811aec2b4fe936cf
Ryan <rpratt1020@gmail.com>
2019-02-17 22:19:47 -0500

    This is the new Player Creation Page

--- app/src/main/java/com/example/spacetrader_bitsplease/entity/Player.java
+++ app/src/main/java/com/example/spacetrader_bitsplease/entity/Player.java
@@ -1,4 +1,58 @@
 package com.example.spacetrader_bitsplease.entity;
 
 public class Player {
+    private String username;
+    private int difficulty;
+    private int pilotSkill;
+    private int engineerSkill;
+    private int fighterSkill;
+    private int traderSkill;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getUsername() {
+        return username;
+    }
+
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public void setPilotSkill(int pilotSkill) {
+        this.pilotSkill = pilotSkill;
+    }
+    public int getPilotSkill() {
+        return pilotSkill;
+    }
+
+    public int getEngineerSkill() {
+        return engineerSkill;
+    }
+    public void setEngineerSkill(int engineerSkill) {
+        this.engineerSkill = engineerSkill;
+    }
+
+    public void setTraderSkill(int traderSkill) {
+        this.traderSkill = traderSkill;
+    }
+    public int getTraderSkill() {
+        return traderSkill;
+    }
+
+    public void setFighterSkill(int fighterSkill) {
+        this.fighterSkill = fighterSkill;
+    }
+    public int getFighterSkill() {
+        return fighterSkill;
+    }
+
+    public String toString() {
+        return String.format("Username: %s \nDifficulty: %s \nPilot Skill: %d \nEngineering Skill: " +
+                "%d \nFighter Skill: %d \nTrader Skill: %d", username, difficulty, pilotSkill,
+                engineerSkill, traderSkill);
+    }
 }
--- app/src/main/java/com/example/spacetrader_bitsplease/views/MainActivity.java
+++ app/src/main/java/com/example/spacetrader_bitsplease/views/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, StartGameActivity.class);
+                Intent intent = new Intent(MainActivity.this, CreatePlayerActivity.class);
                 startActivityForResult(intent, START_REQUEST_ID);
             }
         });
