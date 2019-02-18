2012d3356339bfb97d25a9fa4d0f4aa354744d4a
Ryan <rpratt1020@gmail.com>
2019-02-17 22:21:49 -0500

    This is the new Player Creation Page

---
+++ app/src/main/java/com/example/spacetrader_bitsplease/views/CreatePlayerActivity.java
@@ -0,0 +1,19 @@
+package com.example.spacetrader_bitsplease.views;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import com.example.spacetrader_bitsplease.R;
+
+public class CreatePlayerActivity extends AppCompatActivity{
+    /** an integer used because add a course returns something */
+    public static final int START_REQUEST_ID = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player_creation);
+    }
+}
