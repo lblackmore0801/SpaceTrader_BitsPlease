a1802a86b40abecadf45ae0aad776e844a4c60f4
Lindsey Blackmore <lblackmore0801@gmail.com>
2019-02-18 11:47:19 -0500

    start page background changed to galaxy image

--- app/src/main/java/com/example/spacetrader_bitsplease/views/StartGameActivity.java
+++ app/src/main/java/com/example/spacetrader_bitsplease/views/StartGameActivity.java
@@ -3,7 +3,7 @@ package com.example.spacetrader_bitsplease.views;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-
+//class should be deleted since main page is now linked to create player page
 public class StartGameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
