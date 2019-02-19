1f3b2368a051b893604839a51982c813a6b6758f
Almaze Lema <chupzilemma12@gmail.com>
2019-02-18 22:35:04 -0500

    added difficulty spinner

--- app/src/main/java/com/example/spacetrader_bitsplease/views/CreatePlayerActivity.java
+++ app/src/main/java/com/example/spacetrader_bitsplease/views/CreatePlayerActivity.java
@@ -3,11 +3,14 @@ package com.example.spacetrader_bitsplease.views;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.view.View;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.spacetrader_bitsplease.R;
+import com.example.spacetrader_bitsplease.entity.Difficulty;
 import com.example.spacetrader_bitsplease.entity.Player;
 
 public class CreatePlayerActivity extends AppCompatActivity{
@@ -26,6 +29,12 @@ public class CreatePlayerActivity extends AppCompatActivity{
    */
     private Player player;
 
+    /* ***********************
+    Spinner for difficulty level
+     */
+    private Spinner difficultySpinner;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +44,7 @@ public class CreatePlayerActivity extends AppCompatActivity{
 
         usernameText = findViewById(R.id.username_Text);
 
+        difficultySpinner = findViewById(R.id.difficulty_spinner);
 
 
         player = new Player("Ryan Pratt");
@@ -42,6 +52,13 @@ public class CreatePlayerActivity extends AppCompatActivity{
 
         usernameText.setText(player.getUsername());
 
+
+                /*
+          Set up the adapter to display the class standings in the spinner
+         */
+        ArrayAdapter<Difficulty> adapterCS = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, Difficulty.values());
+        adapterCS.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        difficultySpinner.setAdapter(adapterCS);
 
     }
 
