8b18b7cd20e8771666328f74be1f9a6cce68679d
Ryan <rpratt1020@gmail.com>
2019-02-21 10:30:28 -0500

    This is the new Constructor for creating a player and a difficulty enum.

--- app/src/main/java/com/example/spacetrader_bitsplease/views/CreatePlayerActivity.java
+++ app/src/main/java/com/example/spacetrader_bitsplease/views/CreatePlayerActivity.java
@@ -1,6 +1,5 @@
 package com.example.spacetrader_bitsplease.views;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -19,6 +18,10 @@ public class CreatePlayerActivity extends AppCompatActivity{
     Widgets we will need for binding and getting information
     */
     private TextView usernameText;
+    private TextView fighterSkill;
+    private TextView pilotSkill;
+    private TextView engineeringSkill;
+    private TextView traderSkill;
 
 
     /* ***********************
@@ -34,6 +37,77 @@ public class CreatePlayerActivity extends AppCompatActivity{
 
 
         usernameText = findViewById(R.id.username_Text);
+        fighterSkill = findViewById(R.id.int_fighter_skill);
+        pilotSkill = findViewById(R.id.int_pilot_skill);
+        engineeringSkill = findViewById(R.id.int_engineering_skill);
+        traderSkill = findViewById(R.id.int_trader_skill);
+
+
+        final Button addFighter = findViewById(R.id.AddFighterSkill);
+        addFighter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                player.setFighterSkill(player.getFighterSkill()+1);
+                fighterSkill.setText(Integer.toString(player.getFighterSkill()));
+            }
+        });
+        final Button addPilot = findViewById(R.id.AddPilotSkill);
+        addPilot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                player.setPilotSkill(player.getPilotSkill()+1);
+                pilotSkill.setText(Integer.toString(player.getPilotSkill()));
+            }
+        });
+        final Button addEngineer = findViewById(R.id.AddEngineeringSkill);
+        addEngineer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                player.setEngineerSkill(player.getEngineerSkill()+1);
+                engineeringSkill.setText(Integer.toString(player.getEngineerSkill()));
+            }
+        });
+        final Button addTrader = findViewById(R.id.AddTraderSkill);
+        addTrader.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                player.setTraderSkill(player.getTraderSkill()+1);
+                traderSkill.setText(Integer.toString(player.getTraderSkill()));
+            }
+        });
+        final Button minusFighter = findViewById(R.id.MinusFighterSkill);
+        minusFighter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                player.setFighterSkill(player.getFighterSkill()-1);
+                fighterSkill.setText(Integer.toString(player.getFighterSkill()));
+            }
+        });
+        final Button minusPilot = findViewById(R.id.MinusPilotSkill);
+        minusPilot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                player.setPilotSkill(player.getPilotSkill()-1);
+                pilotSkill.setText(Integer.toString(player.getPilotSkill()));
+            }
+        });
+        final Button minusEngineer = findViewById(R.id.MinusEngineeringSkill);
+        minusEngineer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                player.setEngineerSkill(player.getEngineerSkill()-1);
+                engineeringSkill.setText(Integer.toString(player.getEngineerSkill()));
+            }
+        });
+        final Button minusTrader = findViewById(R.id.MinusTraderSkill);
+        minusTrader.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                player.setTraderSkill(player.getTraderSkill()-1);
+                traderSkill.setText(Integer.toString(player.getTraderSkill()));
+            }
+        });
+
 
 
 
