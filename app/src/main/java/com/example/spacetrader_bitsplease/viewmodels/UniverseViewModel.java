157b224e59eb4ec724710ee93f44b42b759d33d9
klee2123 <klee2123@gmail.com>
2019-03-14 15:45:32 -0400

    universe view model for traveling

---
+++ app/src/main/java/com/example/spacetrader_bitsplease/viewmodels/UniverseViewModel.java
@@ -0,0 +1,33 @@
+package com.example.spacetrader_bitsplease.viewmodels;
+
+import com.example.spacetrader_bitsplease.entity.Ship;
+import com.example.spacetrader_bitsplease.entity.Planet;
+
+public class UniverseViewModel {
+    private static double currentX;
+    private static double currentY;
+    private static double targetX;
+    private static double targetY;
+    private static double distance;
+
+    public static double distance(Planet currentPlanet, Planet targetPlanet) {
+        currentX = currentPlanet.getXcoordinate();
+        currentY = currentPlanet.getYcoordinate();
+        targetX = targetPlanet.getXcoordinate();
+        targetY = targetPlanet.getYcoordinate();
+        distance = Math.sqrt(Math.pow(targetX - currentX, 2) + Math.pow(targetY - currentY, 2));
+        distance = Math.round(distance * 100.00) / 100.00;
+        return distance;
+    }
+
+    public static double fuelRange(Ship ship, int fuel) {
+        return 50;
+    }
+
+    public static boolean inRange(Planet currentPlanet, Planet targetPlanet, Ship ship, int fuel) {
+        if (fuelRange(ship, fuel) >= distance(currentPlanet, targetPlanet)) {
+            return true;
+        }
+        return false;
+    }
+}
