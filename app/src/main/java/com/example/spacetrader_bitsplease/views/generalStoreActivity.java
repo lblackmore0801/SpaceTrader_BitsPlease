8be1afb7575c5197200b5e1da4a4d05b70de955b
Ryan <rpratt1020@gmail.com>
2019-04-08 14:09:13 -0400

    General Store Activity

---
+++ app/src/main/java/com/example/spacetrader_bitsplease/views/generalStoreActivity.java
@@ -0,0 +1,17 @@
+package com.example.spacetrader_bitsplease.views;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+
+import com.example.spacetrader_bitsplease.R;
+
+public class generalStoreActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_general_store);
+    }
+}
