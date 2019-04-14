51b71017b1a2e31273a0158367922a01f2a119df
Enrique Bercian <qbercian@hotmail.com>
2019-04-14 16:00:32 -0400

    Javadocs for MarketPlaceViewModel

--- app/src/main/java/com/example/spacetrader_bitsplease/viewmodels/MarketPlaceViewModel.java
+++ app/src/main/java/com/example/spacetrader_bitsplease/viewmodels/MarketPlaceViewModel.java
@@ -1,24 +1,24 @@
 package com.example.spacetrader_bitsplease.viewmodels;
 
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
+//import android.app.AlertDialog;
+//import android.content.Context;
+//import android.content.DialogInterface;
+//import android.content.Intent;
 
-import com.example.spacetrader_bitsplease.R;
-import com.example.spacetrader_bitsplease.entity.Condition;
+//import com.example.spacetrader_bitsplease.R;
+//import com.example.spacetrader_bitsplease.entity.Condition;
 import com.example.spacetrader_bitsplease.entity.Planet;
 import com.example.spacetrader_bitsplease.entity.Player;
-import com.example.spacetrader_bitsplease.entity.Condition;
-import com.example.spacetrader_bitsplease.entity.TradeGoods;
-import com.example.spacetrader_bitsplease.views.CreatePlayerActivity;
-import com.example.spacetrader_bitsplease.views.PlanetSelectionActivity;
+//import com.example.spacetrader_bitsplease.entity.Condition;
+//import com.example.spacetrader_bitsplease.entity.TradeGoods;
+//import com.example.spacetrader_bitsplease.views.CreatePlayerActivity;
+//import com.example.spacetrader_bitsplease.views.PlanetSelectionActivity;
 
 public class MarketPlaceViewModel {
 
     public static int remainingStorageCapacity = 15;
     //public static int money = 1000;
-    public static int money = Player.getMoney();
+    //private static int money = Player.getMoney();
 
     private static int waterResourceinHold;
     private static int furResourceinHold;
@@ -31,9 +31,14 @@ public class MarketPlaceViewModel {
     private static int narcoticResourceinHold;
     private static int robotResourceinHold;
 
-    public MarketPlaceViewModel(int money, int remainingStorageCapacity) {
-        this.remainingStorageCapacity = remainingStorageCapacity;
-        this.money = Player.getMoney();
+    /**
+     * Constructor for MarketPlaceViewModel
+     * @param  money  not used tbh
+     * @param  remainingStorageCapacity1 storage remaining
+     */
+    public MarketPlaceViewModel(int money, int remainingStorageCapacity1) {
+        remainingStorageCapacity = remainingStorageCapacity1;
+        //money = money;
     }
 
 
@@ -55,13 +60,21 @@ public class MarketPlaceViewModel {
 
     //only buy a resource if you have sufficient funds and storage capacity
 
+
+    /**
+     * Constructor for MarketPlaceViewModel
+     * @param  price price
+     * @param  quantity quantity
+     * @return whether can buy
+     */
     public static boolean canBuy(int price, int quantity) {
-        if (((price * quantity) <= Player.getMoney()) && (remainingStorageCapacity > 0)) {
-            return true;
-        }
-        return false;
+        return (price * quantity) <= Player.getMoney() && remainingStorageCapacity > 0;
     }
 
+    /**
+     * Buy one quantity
+     * @param  currentPlanet current Planet where player is
+     */
     public static void buyOneWater(Planet currentPlanet) {
         if (canBuy(currentPlanet.getWaterPrice(), 1)) {
             Player.setMoney(Player.getMoney() - currentPlanet.getWaterPrice());
@@ -70,6 +83,10 @@ public class MarketPlaceViewModel {
         }
     }
 
+    /**
+     * Buy one quantity
+     * @param  currentPlanet current Planet where player is
+     */
     public static void buyOneFur(Planet currentPlanet) {
         if (canBuy(currentPlanet.getFurPrice(), 1)) {
             Player.setMoney(Player.getMoney() - currentPlanet.getFurPrice());
@@ -78,6 +95,10 @@ public class MarketPlaceViewModel {
         }
     }
 
+    /**
+     * Buy one quantity
+     * @param  currentPlanet current Planet where player is
+     */
     public static void buyOneFood(Planet currentPlanet) {
         if (canBuy(currentPlanet.getFoodPrice(), 1)) {
             Player.setMoney(Player.getMoney() - currentPlanet.getFoodPrice());
@@ -86,6 +107,10 @@ public class MarketPlaceViewModel {
         }
     }
 
+    /**
+     * Buy one quantity
+     * @param  currentPlanet current Planet where player is
+     */
     public static void buyOneOre(Planet currentPlanet) {
         if (canBuy(currentPlanet.getOrePrice(), 1)) {
             Player.setMoney(Player.getMoney() - currentPlanet.getOrePrice());
@@ -94,6 +119,10 @@ public class MarketPlaceViewModel {
         }
     }
 
+    /**
+     * Buy one quantity
+     * @param  currentPlanet current Planet where player is
+     */
     public static void buyOneGame(Planet currentPlanet) {
         if (canBuy(currentPlanet.getGamePrice(), 1)) {
             Player.setMoney(Player.getMoney() - currentPlanet.getGamePrice());
@@ -102,6 +131,10 @@ public class MarketPlaceViewModel {
         }
     }
 
+    /**
+     * Buy one quantity
+     * @param  currentPlanet current Planet where player is
+     */
     public static void buyOneFirearm(Planet currentPlanet) {
         if (canBuy(currentPlanet.getFirearmPrice(), 1)) {
             Player.setMoney(Player.getMoney() - currentPlanet.getFirearmPrice());
@@ -110,6 +143,10 @@ public class MarketPlaceViewModel {
         }
     }
 
+    /**
+     * Buy one quantity
+     * @param  currentPlanet current Planet where player is
+     */
     public static void buyOneMedicine(Planet currentPlanet) {
         if (canBuy(currentPlanet.getMedicinePrice(), 1)) {
             Player.setMoney(Player.getMoney() - currentPlanet.getMedicinePrice());
@@ -118,6 +155,10 @@ public class MarketPlaceViewModel {
         }
     }
 
+    /**
+     * Buy one quantity
+     * @param  currentPlanet current Planet where player is
+     */
     public static void buyOneMachine(Planet currentPlanet) {
         if (canBuy(currentPlanet.getMachinePrice(), 1)) {
             Player.setMoney(Player.getMoney() - currentPlanet.getMachinePrice());
@@ -126,6 +167,10 @@ public class MarketPlaceViewModel {
         }
     }
 
+    /**
+     * Buy one quantity
+     * @param  currentPlanet current Planet where player is
+     */
     public static void buyOneNarcotic(Planet currentPlanet) {
         if (canBuy(currentPlanet.getNarcoticPrice(), 1)) {
             Player.setMoney(Player.getMoney() - currentPlanet.getNarcoticPrice());
@@ -134,6 +179,10 @@ public class MarketPlaceViewModel {
         }
     }
 
+    /**
+     * Buy one quantity
+     * @param  currentPlanet current Planet where player is
+     */
     public static void buyOneRobot(Planet currentPlanet) {
         if (canBuy(currentPlanet.getRobotPrice(), 1)) {
             Player.setMoney(Player.getMoney() - currentPlanet.getRobotPrice());
@@ -146,6 +195,10 @@ public class MarketPlaceViewModel {
     * Selling stuff
      */
 
+    /**
+     * Sell one quantity
+     * @param  currentPlanet current Planet where player is
+     */
     public static void sellWater(Planet currentPlanet) {
         if (waterResourceinHold > 0 && remainingStorageCapacity >= 0) {
             int currentWaterQuant = currentPlanet.getWaterQuant();
@@ -156,6 +209,10 @@ public class MarketPlaceViewModel {
         }
     }
 
+    /**
+     * Sell one quantity
+     * @param  currentPlanet current Planet where player is
+     */
     public static void sellFur(Planet currentPlanet) {
         if (furResourceinHold > 0 && remainingStorageCapacity >= 0) {
             int currentFurQuant = currentPlanet.getFurQuant();
@@ -166,6 +223,10 @@ public class MarketPlaceViewModel {
         }
     }
 
+    /**
+     * Sell one quantity
+     * @param  currentPlanet current Planet where player is
+     */
     public static void sellFood(Planet currentPlanet) {
         if (foodResourceinHold > 0 && remainingStorageCapacity >= 0) {
             int currentFoodQuant = currentPlanet.getFoodQuant();
@@ -176,6 +237,10 @@ public class MarketPlaceViewModel {
         }
     }
 
+    /**
+     * Sell one quantity
+     * @param  currentPlanet current Planet where player is
+     */
     public static void sellOre(Planet currentPlanet) {
         if (oreResourceinHold > 0 && remainingStorageCapacity >= 0) {
             int currentOreQuant = currentPlanet.getOreQuant();
@@ -186,6 +251,10 @@ public class MarketPlaceViewModel {
         }
     }
 
+    /**
+     * Sell one quantity
+     * @param  currentPlanet current Planet where player is
+     */
     public static void sellGame(Planet currentPlanet) {
         if (gameResourceinHold > 0 && remainingStorageCapacity >= 0) {
             int currentGameQuant = currentPlanet.getGameQuant();
@@ -196,6 +265,10 @@ public class MarketPlaceViewModel {
         }
     }
 
+    /**
+     * Sell one quantity
+     * @param  currentPlanet current Planet where player is
+     */
     public static void sellFirearm(Planet currentPlanet) {
         if (firearmsResourceinHold > 0 && remainingStorageCapacity >= 0) {
             int currentFirearmQuant = currentPlanet.getFirearmQuant();
@@ -206,6 +279,10 @@ public class MarketPlaceViewModel {
         }
     }
 
+    /**
+     * Sell one quantity
+     * @param  currentPlanet current Planet where player is
+     */
     public static void sellMedicine(Planet currentPlanet) {
         if (medicineResourceinHold > 0 && remainingStorageCapacity >= 0) {
             int currentMedicineQuant = currentPlanet.getMedicineQuant();
@@ -216,6 +293,10 @@ public class MarketPlaceViewModel {
         }
     }
 
+    /**
+     * Sell one quantity
+     * @param  currentPlanet current Planet where player is
+     */
     public static void sellMachine(Planet currentPlanet) {
         if (machineResourceinHold > 0 && remainingStorageCapacity >= 0) {
             int currentMachineQuant = currentPlanet.getMachineQuant();
@@ -226,6 +307,10 @@ public class MarketPlaceViewModel {
         }
     }
 
+    /**
+     * Sell one quantity
+     * @param  currentPlanet current Planet where player is
+     */
     public static void sellNarcotic(Planet currentPlanet) {
         if (narcoticResourceinHold > 0 && remainingStorageCapacity >= 0) {
             int currentNarcoticQuant = currentPlanet.getNarcoticQuant();
@@ -236,6 +321,10 @@ public class MarketPlaceViewModel {
         }
     }
 
+    /**
+     * Sell one quantity
+     * @param  currentPlanet current Planet where player is
+     */
     public static void sellRobot(Planet currentPlanet) {
         if (robotResourceinHold > 0 && remainingStorageCapacity >= 0) {
             int currentRobotQuant = currentPlanet.getRobotQuant();
@@ -246,94 +335,186 @@ public class MarketPlaceViewModel {
         }
     }
 
+    /**
+     * Getter for inHoldMaterial
+     * @return int of materials
+     */
     public static int getWaterResourceinHold() {
         return waterResourceinHold;
     }
 
+    /**
+     * Getter for inHoldMaterial
+     * @return int of materials
+     */
     public static int getFirearmsResourceinHold() {
         return firearmsResourceinHold;
     }
 
+    /**
+     * Getter for inHoldMaterial
+     * @return int of materials
+     */
     public static int getFoodResourceinHold() {
         return foodResourceinHold;
     }
 
+    /**
+     * Getter for inHoldMaterial
+     * @return int of materials
+     */
     public static int getFurResourceinHold() {
         return furResourceinHold;
     }
 
+    /**
+     * Getter for inHoldMaterial
+     * @return int of materials
+     */
     public static int getOreResourceinHold() {
         return oreResourceinHold;
     }
 
+    /**
+     * Getter for inHoldMaterial
+     * @return int of materials
+     */
     public static int getGameResourceinHold() {
         return gameResourceinHold;
     }
 
+    /**
+     * Getter for inHoldMaterial
+     * @return int of materials
+     */
     public static int getMachineResourceinHold() {
         return machineResourceinHold;
     }
 
+    /**
+     * Getter for inHoldMaterial
+     * @return int of materials
+     */
     public static int getMedicineResourceinHold() {
         return medicineResourceinHold;
     }
 
+    /**
+     * Getter for inHoldMaterial
+     * @return int of materials
+     */
     public static int getNarcoticResourceinHold() {
         return narcoticResourceinHold;
     }
 
-    public static int getRemainingStorageCapacity() {
-        return remainingStorageCapacity;
-    }
+    ///**
+    // * Getter for inHoldMaterial
+    // * @return int of materials
+    // */
+    //public static int getRemainingStorageCapacity() {
+    //    return remainingStorageCapacity;
+    //}
 
+    /**
+     * Getter for inHoldMaterial
+     * @return int of materials
+     */
     public static int getRobotResourceinHold() {
         return robotResourceinHold;
     }
 
-    public static int getMoney() {
-        return money;
-    }
+    ///**
+    // * Getter for inHoldMaterial
+    // * @return int of materials
+    // */
+    //public static int getMoney() {
+    //    return money;
+    //}
 
+    /**
+     * Setter for inHoldMaterial
+     * @param firearmsResourceinHold set material to this quantity
+     */
     public static void setFirearmsResourceinHold(int firearmsResourceinHold) {
         MarketPlaceViewModel.firearmsResourceinHold = firearmsResourceinHold;
     }
 
+    /**
+     * Setter for inHoldMaterial
+     * @param foodResourceinHold set material to this quantity
+     */
     public static void setFoodResourceinHold(int foodResourceinHold) {
         MarketPlaceViewModel.foodResourceinHold = foodResourceinHold;
     }
 
+    /**
+     * Setter for inHoldMaterial
+     * @param furResourceinHold set material to this quantity
+     */
     public static void setFurResourceinHold(int furResourceinHold) {
         MarketPlaceViewModel.furResourceinHold = furResourceinHold;
     }
 
+    /**
+     * Setter for inHoldMaterial
+     * @param gameResourceinHold set material to this quantity
+     */
     public static void setGameResourceinHold(int gameResourceinHold) {
         MarketPlaceViewModel.gameResourceinHold = gameResourceinHold;
     }
 
+    /**
+     * Setter for inHoldMaterial
+     * @param machineResourceinHold set material to this quantity
+     */
     public static void setMachineResourceinHold(int machineResourceinHold) {
         MarketPlaceViewModel.machineResourceinHold = machineResourceinHold;
     }
 
+    /**
+     * Setter for inHoldMaterial
+     * @param medicineResourceinHold set material to this quantity
+     */
     public static void setMedicineResourceinHold(int medicineResourceinHold) {
         MarketPlaceViewModel.medicineResourceinHold = medicineResourceinHold;
     }
 
+    /**
+     * Setter for inHoldMaterial
+     * @param narcoticResourceinHold set material to this quantity
+     */
     public static void setNarcoticResourceinHold(int narcoticResourceinHold) {
         MarketPlaceViewModel.narcoticResourceinHold = narcoticResourceinHold;
     }
 
+    /**
+     * Setter for inHoldMaterial
+     * @param oreResourceinHold set material to this quantity
+     */
     public static void setOreResourceinHold(int oreResourceinHold) {
         MarketPlaceViewModel.oreResourceinHold = oreResourceinHold;
     }
 
+    /**
+     * Setter for inHoldMaterial
+     * @param robotResourceinHold set material to this quantity
+     */
     public static void setRobotResourceinHold(int robotResourceinHold) {
         MarketPlaceViewModel.robotResourceinHold = robotResourceinHold;
     }
 
+    /**
+     * Setter for inHoldMaterial
+     * @param remainingStorageCapacity set material to this quantity
+     */
     public static void setRemainingStorageCapacity(int remainingStorageCapacity) {
         MarketPlaceViewModel.remainingStorageCapacity = remainingStorageCapacity;
     }
 
+    /**
+     * Setter for inHoldMaterial
+     * @param waterResourceinHold set material to this quantity
+     */
     public static void setWaterResourceinHold(int waterResourceinHold) {
         MarketPlaceViewModel.waterResourceinHold = waterResourceinHold;
     }
