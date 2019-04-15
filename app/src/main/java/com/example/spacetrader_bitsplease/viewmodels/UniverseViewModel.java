7a7da1a679bcd77afc898d233e975bdc5e66b837
klee2123 <klee2123@gmail.com>
2019-04-15 10:14:03 -0400

    javadocs

--- app/src/main/java/com/example/spacetrader_bitsplease/viewmodels/UniverseViewModel.java
+++ app/src/main/java/com/example/spacetrader_bitsplease/viewmodels/UniverseViewModel.java
@@ -13,6 +13,11 @@ public class UniverseViewModel {
     private static double targetY;
     private static double distance;
 
+    /**
+     * Constructor for UniverseViewModel
+     * @param  currentPlanet the current planet the player is on
+     * @param  targetPlanet the planet the player wishes to travel to
+     */
     public static double distance(Planet currentPlanet, Planet targetPlanet) {
         currentX = currentPlanet.getXcoordinate();
         currentY = currentPlanet.getYcoordinate();
@@ -23,10 +28,20 @@ public class UniverseViewModel {
         return distance;
     }
 
+    /**
+     * Method for getting the current fuel range of the ship.
+     * @param  ship  the player's ship
+     * @return an int denoting fuel value
+     */
     public static int fuelRange(Ship ship) {
         return ship.getFuel();
     }
 
+    /**
+     * Method to check if the target planet is in travelling range.
+     * @param  ship  the player's ship
+     * @return boolean can travel or not
+     */
     public static boolean inRange(Planet currentPlanet, Planet targetPlanet, Ship ship) {
         if (fuelRange(ship) >= distance(currentPlanet, targetPlanet)) {
             ship.decrementFuel(distance);
@@ -35,6 +50,10 @@ public class UniverseViewModel {
         return false;
     }
 
+    /**
+     * Method to check if the player encounters a cop.
+     * @return boolean run into cop or not
+     */
     public static boolean copEncounter() {
         int random = (int) (Math.random() * 100 + 1);
         if (random > 10) {
@@ -42,7 +61,10 @@ public class UniverseViewModel {
         }
         return false;
     }
-
+    /**
+     * Method to check if the player has illegal items in their hold.
+     * @return boolean if the player has illegal items or not
+     */
     public static boolean checkForIllegals() {
         if (MarketPlaceViewModel.getNarcoticResourceinHold() > 0 ||
             MarketPlaceViewModel.getFirearmsResourceinHold() > 0) {
