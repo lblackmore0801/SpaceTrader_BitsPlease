ba83e029124c6e78cf0d49abaf50f409fc51b22d
klee2123 <klee2123@gmail.com>
2019-04-15 15:14:27 -0400

    getting rid of warnings

--- app/src/main/java/com/example/spacetrader_bitsplease/views/generalStoreActivity.java
+++ app/src/main/java/com/example/spacetrader_bitsplease/views/generalStoreActivity.java
@@ -1,10 +1,8 @@
 package com.example.spacetrader_bitsplease.views;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
-import android.view.View;
+
 
 import com.example.spacetrader_bitsplease.R;
 
