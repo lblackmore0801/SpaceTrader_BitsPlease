a5ea283fe2e6f1dc13eadf0a64c61fc2c310d2c9
Lindsey Blackmore <lblackmore0801@gmail.com>
2019-04-15 09:06:58 -0400

    javadocs for half of entity folder

--- app/src/main/java/com/example/spacetrader_bitsplease/entity/PlanetName.java
+++ app/src/main/java/com/example/spacetrader_bitsplease/entity/PlanetName.java
@@ -14,10 +14,18 @@ public enum PlanetName {
 
     private final String planetname;
 
+    /**
+     * single parameter constructor for the name of a planet
+     * @param planetname String representation of a the planet name
+     */
     PlanetName(String planetname) {
         this.planetname = planetname;
     }
 
+    /**
+     * toString method for the planet name
+     * @return String representation of the planet name
+     */
     public String toString() {
         return planetname;
     }
--- app/src/main/java/com/example/spacetrader_bitsplease/entity/Player.java
+++ app/src/main/java/com/example/spacetrader_bitsplease/entity/Player.java
@@ -10,41 +10,29 @@ public class Player {
     private static Ship ship;
     private static int money;
 
-    private int waterResourceinHold;
-    private int furResourceinHold;
-    private int foodResourceinHold;
-    private int oreResourceinHold;
-    private int gameResourceinHold;
-    private int firearmsResourceinHold;
-    private int medicineResourceinHold;
-    private int narcoticResourceinHold;
-    private int robotResourceinHold;
-
-
-
     /**
      * Constructor required for making the new object
      *
      * @param name name of the student
      */
     public Player(String name) {
-        this(name, Difficulty.EASY, 0,0,0,0, new Ship(), 1000);
+        this(name, Difficulty.EASY, 0, 0, 0, 0, new Ship(), 1000);
     }
 
 
     /**
      * Constructor required for making the new object
      *
-     * @param name name of the student
-     * @param difficulty Difficulty level
-     * @param pilotSkill pilot skill level
-     * @param fighterSkill fighter skill level
+     * @param name          name of the student
+     * @param difficulty    Difficulty level
+     * @param pilotSkill    pilot skill level
+     * @param fighterSkill  fighter skill level
      * @param engineerSkill engineering skill level
-     * @param traderSkill trader skill level
-     * @param ship type of ship
-     * @param money starting money
+     * @param traderSkill   trader skill level
+     * @param ship          type of ship
+     * @param money         starting money
      */
-    public Player(String name, Difficulty difficulty, int pilotSkill, int engineerSkill,
+    private Player(String name, Difficulty difficulty, int pilotSkill, int engineerSkill,
                   int fighterSkill, int traderSkill, Ship ship, int money) {
         this.username = name;
         this.difficulty = difficulty;
@@ -56,85 +44,120 @@ public class Player {
         this.money = money;
     }
 
+    /**
+     * setter of the username of a player
+     * @param name String representation of the username
+     */
     public void setUsername(String name) {
         this.username = name;
     }
 
+    /**
+     * getter for the username
+     * @return String representation of the username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * setter for the game difficulty
+     * @param difficulty Difficulty enum for the game difficulty
+     */
     public void setDifficulty(Difficulty difficulty) {
         this.difficulty = difficulty;
     }
-    public Difficulty getDifficulty() {
-        return difficulty;
-    }
 
+    /**
+     * setter for the pilot skill
+     * @param pilotSkill int representation of the pilot skill level
+     */
     public void setPilotSkill(int pilotSkill) {
         this.pilotSkill = pilotSkill;
     }
+
+    /**
+     * getter for the pilot skill
+     * @return int representation of the pilot skill
+     */
     public int getPilotSkill() {
         return pilotSkill;
     }
 
+    /**
+     * getter for the engineering skill
+     * @return int representation of the engineering skill
+     */
     public int getEngineerSkill() {
         return engineerSkill;
     }
+
+    /**
+     * setter for the engineering skill
+     * @param engineerSkill int representation of the engineering skill
+     */
     public void setEngineerSkill(int engineerSkill) {
         this.engineerSkill = engineerSkill;
     }
 
+    /**
+     * setter for the trader skill
+     * @param traderSkill int representation of the trader skill
+     */
     public void setTraderSkill(int traderSkill) {
         this.traderSkill = traderSkill;
     }
+
+    /**
+     * getter for the trader skill
+     * @return int representation of the trader skill
+     */
     public int getTraderSkill() {
         return traderSkill;
     }
 
+    /**
+     * setter for the fighter skill
+     * @param fighterSkill int representation of the trader skill
+     */
     public void setFighterSkill(int fighterSkill) {
         this.fighterSkill = fighterSkill;
     }
+
+    /**
+     * getter for the fighter skill
+     * @return int representation of the fighter skill
+     */
     public int getFighterSkill() {
         return fighterSkill;
     }
 
-    public static void setMoney(int newMoney) { money = newMoney; }
-    public static int getMoney() { return money; }
+    /**
+     * setter for money
+     * @param newMoney int representation of money
+     */
+    public static void setMoney(int newMoney) {
+        money = newMoney;
+    }
 
-    public void setShip(Ship ship) { this.ship = ship; }
-    public static Ship getShip() { return ship; }
+    /**
+     * getter for the money
+     * @return int representation of money
+     */
+    public static int getMoney() {
+        return money;
+    }
+
+    /**
+     * getter for the ship
+     * @return Ship enum representation of the ship
+     */
+    public static Ship getShip() {
+        return ship;
+    }
 
     public String toString() {
         return String.format("Username: %s \nDifficulty: %s \nFighter Skill: %s \nPilot Skill: %s \nEngineering Skill: %s \nTrader Skill: %s \nShip: %s \nMoney: %d",
-                                username, difficulty.toString(), fighterSkill, pilotSkill, engineerSkill, traderSkill, ship.toString(), money);
+                username, difficulty.toString(), fighterSkill, pilotSkill, engineerSkill, traderSkill, ship.toString(), money);
     }
-
-
-    public void setWaterinHold(int waterResourceinHold) { this.waterResourceinHold = waterResourceinHold; }
-    public int getWaterinHold() { return waterResourceinHold; }
-
-    public void setFoodinHold(int foodResourceinHold) { this.foodResourceinHold = foodResourceinHold; }
-    public int setFoodinHold() { return foodResourceinHold; }
-
-    public void setFurResourceinHold(int furResourceinHold) { this.furResourceinHold = furResourceinHold; }
-    public int getFurResourceinHold() { return furResourceinHold; }
-
-    public void setOreResourceinHold(int oreResourceinHold) { this.oreResourceinHold = oreResourceinHold; }
-    public int getOreResourceinHold() { return oreResourceinHold; }
-
-    public void setGameResourceinHold(int gameResourceinHold) { this.gameResourceinHold = gameResourceinHold; }
-    public int getGameResourceinHold() { return gameResourceinHold; }
-
-    public void setFirearmsResourceinHold(int firearmsResourceinHold) { this.firearmsResourceinHold = firearmsResourceinHold; }
-    public int getFirearmsResourceinHold() { return firearmsResourceinHold; }
-
-    public void setMedicineResourceinHold(int medicineResourceinHold) { this.medicineResourceinHold = medicineResourceinHold; }
-    public int getMedicineResourceinHold() { return medicineResourceinHold; }
-
-    public void setNarcoticResourceinHold(int narcoticResourceinHold) { this.narcoticResourceinHold = narcoticResourceinHold; }
-    public int getNarcoticResourceinHold() { return narcoticResourceinHold; }
-
-    public void setRobotResourceinHold(int robotResourceinHold) { this.robotResourceinHold = robotResourceinHold; }
-    public int getRobotResourceinHold() { return robotResourceinHold; }
 }
